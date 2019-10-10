02158ceaaaf21007794e55766040b06e91a56ca2
Sebastian Felix Schwarz <schwarz@nexus-netsoft.com>
2019-10-10 13:30:44 +0200

    init prj

--- handler.js
+++ handler.js
@@ -1,12 +1,9 @@
 'use strict';
 const AWS = require('aws-sdk');
 const moment = require('moment-timezone');
-const s3 = new AWS.S3({region: 'eu-central-1'});
 const axios = require('axios');
 
-const BUCKET = process.env.BUCKET;
-const BLYNK = process.env.BLYNK;
-
+const STATE = process.env.STATE;
 
 async function writePin(pin,val) {
   try {
@@ -16,85 +13,27 @@ async function writePin(pin,val) {
     console.error(error);
   }
 }
-function mapUser2Pin(dev){
-    switch (dev) {
-      case "Hellboy": return "V1";
-      case "Nora": return "V3";
-      case "Guest1": return "V5";
-      case "Guest2": return "V7";
-    }
-}
 
-
-module.exports.air = async (event) => {
+module.exports.vacation = async (event) => {
   console.log(event);
   const now = moment().tz("Europe/Berlin").format();
-  let obj = {
-    temp : parseFloat(event.temp),
-    hum : parseFloat(event.hum),
-    hic : parseFloat(event.hic),
-    gas : parseFloat(event.gas),
-    room : event.room,
-    update: now
-  };
-  let filename = obj.room+'.json';
-  let data = await s3.getObject({
-    Bucket: BUCKET,
-    Key: filename
-  }).promise();
-  let temp = JSON.parse(data.Body.toString());
-  if(parseFloat(temp.maxTemp) < obj.temp){
-    obj.maxTemp = obj.temp;
-    obj.maxTempTS = now;
-  }else{
-    obj.maxTemp = temp.maxTemp;
-    obj.maxTempTS = temp.maxTempTS;
-  }
-  if(parseFloat(temp.minTemp)<obj.temp){
-    obj.minTemp = temp.minTemp;
-    obj.minTempTS = temp.minTempTS;
-  }else{
-    obj.minTemp = obj.temp;
-    obj.minTempTS = now;
-  }
-  if(parseFloat(temp.maxHum) < obj.hum){
-    obj.maxHum = obj.hum;
-    obj.maxHumTS = now;
-  }else{
-    obj.maxHum = temp.maxHum;
-    obj.maxHumTS = temp.maxHumTS;
-  }
-  if(parseFloat(temp.maxHic) < obj.hic){
-    obj.maxHic = obj.hic;
-    obj.maxHicTS = now;
-  }else{
-    obj.maxHic = temp.maxHic;
-    obj.maxHicTS = temp.maxHicTS;
-  }
-  if(parseFloat(temp.maxGas) < obj.gas){
-    obj.maxGas = obj.gas;
-    obj.maxGasTS = now;
-  }else{
-    obj.maxGas = temp.maxGas;
-    obj.maxGasTS = temp.maxGasTS;
+  let isVacation = false;
+  let vacationName = 'None';
+
+  const vacations = await axios.get('https://ferien-api.de/api/v1/holidays/'+STATE+'/'+moment().year());
+
+  for(let i in vacations.data){
+    if(moment(now).isBetween(vacations.data[i].start, vacations.data[i].end)){
+      isVacation = true;
+      vacationName = vacations.data[i].name,
+      console.log(" F E R I E N ");
+      console.log(vacationName);
+    }
   }
 
-  let bufferObject = new Buffer.from(JSON.stringify(obj));
-  let s3params = {
-    Bucket: BUCKET,
-    Body : bufferObject,
-    Key : filename,
-    CacheControl:'public, max-age=60',
-    ContentType: "application/json"
-  };
-
-  const key = await s3.upload(s3params).promise();
- // const blynk = await writePin(mapUser2Pin(obj.device),obj.last);
   return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: JSON.stringify(key),
-      input: event,
-    }),
+
+      message: isVacation,
+      vacation: vacationName
   };
 };
